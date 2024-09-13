a427af181b491ef9e4477907eb8c129c1015c95c
julia-stevens <juliastevens02@gmail.com>
2024-09-13 11:23:14 +0200

    bron js

--- scripts/julia-script.js
+++ scripts/julia-script.js
@@ -1,3 +1,5 @@
+// Bron: https://www.w3schools.com/howto/howto_js_filter_lists.asp
+
 function myFunction() {
     var input, filter, ul, li, h3, i, txtValue;
     input = document.getElementById("myInput");
