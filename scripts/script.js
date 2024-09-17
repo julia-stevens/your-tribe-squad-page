3ba9fa199501b4b34dfe3ee32bb5ffd50f51a20a
ambersr <amber.schalker@hva.nl>
2024-09-17 12:57:57 +0200

    Code samengevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -42,15 +42,36 @@ const prevButton = document.querySelector('.carousel-prev');
     });
 
 
+// function myFunction() {
+//     var input, filter, ul, li, h2, i, txtValue;
+//     input = document.getElementById("myInput");
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName("li");
+//     for (i = 0; i < li.length; i++) {
+//         h2 = li[i].getElementsByTagName("h2")[0];
+//         txtValue = h2.textContent || h2.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
+
+
+// Bron: https://www.w3schools.com/howto/howto_js_filter_lists.asp
+
+// Zoek / filter functie
 function myFunction() {
-    var input, filter, ul, li, h2, i, txtValue;
+    var input, filter, ul, li, h3, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
-        h2 = li[i].getElementsByTagName("h2")[0];
-        txtValue = h2.textContent || h2.innerText;
+        h3 = li[i].getElementsByTagName("h3")[0];
+        txtValue = h3.textContent || h3.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
