53b42fbbe28d8ca5b4f52edc32319797c76120d2
ambersr <amber.schalker@hva.nl>
2024-09-17 11:36:24 +0200

    Laatste versie code van Amber

--- scripts/script.js
+++ scripts/script.js
@@ -33,3 +33,28 @@ const prevButton = document.querySelector('.carousel-prev');
         const itemsPerView = window.innerWidth >= 768 ? 3 : 1;
         carouselList.style.transform = `translateX(-${index * itemWidth}px)`;
     });
+
+     // Optional: Add a resize event listener to update itemWidth and itemsPerView when window size changes
+    window.addEventListener('resize', () => {
+        const itemWidth = carouselItems[0].getBoundingClientRect().width;
+        const itemsPerView = window.innerWidth >= 1000 ? 2 : 1;
+        carouselList.style.transform = `translateX(-${index * itemWidth}px)`;
+    });
+
+
+function myFunction() {
+    var input, filter, ul, li, h2, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        h2 = li[i].getElementsByTagName("h2")[0];
+        txtValue = h2.textContent || h2.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
