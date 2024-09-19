6418afb512cc585d22f9ca5a38c937966577a26c
julia-stevens <juliastevens02@gmail.com>
2024-09-19 10:25:08 +0200

    merge fix en extra's

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,34 @@
+// Bron: https://www.w3schools.com/howto/howto_js_filter_lists.asp
+function myFunction() {
+    var input, filter, ul, li, h3, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        h3 = li[i].getElementsByTagName("h3")[0];
+        txtValue = h3.textContent || h3.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+const duck = document.querySelector("#duck")
+duck.addEventListener("click", function() {
+    duck.classList.toggle("active")    
+});
+
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
     spaceBetween: 100,
+    centeredSlides: true,
+    
   
     // Navigation arrows
     navigation: {
@@ -24,21 +51,4 @@ const swiper = new Swiper('.swiper', {
 
   });
 
-// Bron: https://www.w3schools.com/howto/howto_js_filter_lists.asp
-function myFunction() {
-    var input, filter, ul, li, h3, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        h3 = li[i].getElementsByTagName("h3")[0];
-        txtValue = h3.textContent || h3.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
+  
